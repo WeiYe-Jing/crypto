d5115b38ac043bc25cfaa6b40dad0581ecc0e497
景文凯 <jingwk@greatld.com>
2019-10-21 13:57:38 +0800

    Default Changelist

--- src/main/java/com/apelearn/cryto/model/Category.java
+++ src/main/java/com/apelearn/cryto/model/Category.java
@@ -1,15 +1,21 @@
 package com.apelearn.cryto.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 
 @Data
 public class Category {
+
+    @ApiModelProperty("ID")
     private String id;
 
+    @ApiModelProperty("分类名称")
     private String categoryName;
 
+    @ApiModelProperty("创建时间")
     private Long createTime;
 
+    @ApiModelProperty("创建用户ID")
     private Long createUserId;
 }
